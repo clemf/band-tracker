bde81b6835f42b5cf7e5f52f7bd5d2c5ed5cdbd0
Clem <clemfree@gmail.com>
2015-01-30 14:14:26 -0800

    Duplicate validation functionality for venue names

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,3 +1,8 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many :bands
+  validates :name, length: { minimum: 1 }
+
+  before_save do
+    self.name = name.downcase.titleize    
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -3,3 +3,17 @@ require 'spec_helper'
 describe Venue do
   it { should have_and_belong_to_many :bands }
 end
+
+describe Venue do
+  it 'prevents input of empty venue name' do
+    test = Venue.create(name: "")
+    expect(test.valid?).to eq false
+  end
+end
+
+describe Venue do
+  it 'capitalizes first letter of venue name' do
+    test = Venue.create(name: "rose quarter")
+    expect(test.name).to eq "Rose Quarter"
+  end
+end
